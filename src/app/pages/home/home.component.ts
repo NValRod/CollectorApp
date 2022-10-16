4f1f636ff785308f2eb0b7429d50a91fc2ecf75e
Nicolas <nicolasvalbuenaro@gmail.com>
2022-10-15 20:07:09 -0500

    Dashboard_Tables

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,25 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/services/auth.services';
+
+export interface userData{
+  nameCompany:string;
+  name: string;
+  id: number;
+  phoneNumber: number;
+  email: string;
+  debt:number;
+  lastDatePay:string;
+}
+
+const ELEMENT_DATA: userData[] = [
+  {nameCompany:'Davivienda',  id: 52054628, name: 'CLAUDIA PATRICIA DE VERA', phoneNumber: 3001234567, email: 'test@gmail.com', debt: 343000, lastDatePay:'19/01/2017'},
+  {nameCompany:'Davivienda',  id: 71950952, name: 'GIL ESCOBAR ALBERTO ', phoneNumber: 3001234567, email: 'test@gmail.com', debt: 360000 , lastDatePay:'20/06/2019'},
+  {nameCompany:'Davivienda',  id: 55249319, name: 'COBA TORRES OLGA LONAYL', phoneNumber: 3001234561, email: 'test@gmail.com', debt: 250000, lastDatePay:'25/01/2015'},
+  {nameCompany:'Davivienda',  id: 80165284, name: 'CESAR AUGUSTO JIMENEZ ARENAS', phoneNumber: 3001234234, email: 'test@gmail.com', debt: 450000, lastDatePay:'26/10/2015'},
+  {nameCompany:'Davivienda',  id: 32843351, name: 'YENIYS GRANADOS MEZA', phoneNumber: 3001234567, email: 'test@gmail.com', debt: 900000, lastDatePay:'26/10/2015'},
+  {nameCompany:'Davivienda',  id: 52461316, name: 'CONTRERAS MORENO MONICA  MARCELA', phoneNumber: 3001234567, email: 'test@gmail.com', debt: 343000, lastDatePay:'26/10/2015'},
+  {nameCompany:'Davivienda',  id: 57423947, name: 'LINDA VIÑAS PEREZ', phoneNumber: 3001234567, email: 'test@gmail.com', debt: 820000, lastDatePay:'26/10/2015'},
+];
 
 @Component({
   selector: 'app-home',
@@ -7,8 +28,17 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent  {
 
-  constructor() { }
+  user$ = this.authSvc.user$;
+  displayedColumns: string[] = ['nameCompany','cedula', 'name', 'phoneNumber', 'email','debt','lastDatePay'];
+  dataSource = ELEMENT_DATA;
 
-
+  constructor(private readonly authSvc:AuthService) { }
+  async onLogOut():Promise<void>{
+    try {
+      await this.authSvc.signOut();
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,21 +5,25 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
 import { FormModule } from '../auth/form/form.module';
-import { SideNavbarComponent } from './side-navbar/side-navbar.component';
-import { SearcherComponent } from './searcher/searcher.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table' 
+
  
 
 @NgModule({
   declarations: [
     HomeComponent,
-    SideNavbarComponent,
-    SearcherComponent,
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     RouterModule,
     FormModule,
+
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule
   ]
 })
 export class HomeModule { }
